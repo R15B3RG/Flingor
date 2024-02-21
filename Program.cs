4092af3599496ba2434d0bf8ce84cd2a1c73840b
R15B3RG <143381712+R15B3RG@users.noreply.github.com>
2024-02-21 16:20:06 +0100

    Update

--- Program.cs
+++ Program.cs
@@ -22,7 +22,22 @@ app.UseHttpsRedirection();
 
 app.MapGet("api/dog", (DayCareContext context) =>
 {
-    return context.Dog;
+    var dogs = new List<Dog>
+        (
+            context.Dog.Select(
+                d => new Dog()
+                {
+                    Name = d.Name,
+                    Owners = new List<Owner>
+                    (
+                        d.Owners.Select(o => new Owner()
+                        {
+                            Name = o.Name
+                        }))
+                }
+                )
+        );
+    return dogs;
 });
 
 app.MapPost("api/dog", (DayCareContext context, Dog dog) =>
@@ -31,13 +46,93 @@ app.MapPost("api/dog", (DayCareContext context, Dog dog) =>
     context.SaveChanges();
 });
 
-app.MapDelete("api/dog", (DayCareContext context, Dog dog) =>
+app.MapDelete("api/dog/{id}", (DayCareContext context, int id) =>
 {
-    context.Dog.Remove(dog);
+    var dog = context.Dog.Find(id);
+    if (dog != null)
+    {
+        context.Dog.Remove(dog);
+        context.SaveChanges();
+    }
+    
+});
+
+app.MapPut("api/dog/{id}", (DayCareContext context, Dog newDog, int id) =>
+{
+    var dog = context.Dog.Find(id);
+
+    if (dog != null)
+    {
+        dog = newDog;
+    }
+
     context.SaveChanges();
 });
 
+app.MapPut("api/dog/owner", (DayCareContext context, int dogId, int ownerId) =>
+{
+    var owner = context.Owner.Find(ownerId);
+    var dog = context.Dog.Find(dogId);
 
+    if (owner != null)
+    {
+        if (dog != null)
+        {
+            dog.Owners.Add(owner);
+            context.SaveChanges();
+        }
+    }
+});
+
+/////////////
+app.MapGet("api/owner", (DayCareContext context) =>
+{
+    return context.Owner;
+});
+
+app.MapPost("api/owner", (DayCareContext context, Owner owner) =>
+{
+    context.Owner.Add(owner);
+    context.SaveChanges();
+});
+
+app.MapDelete("api/owner/{id}", (DayCareContext context, int id) =>
+{
+    var owner = context.Owner.Find(id);
+    if (owner != null)
+    {
+        context.Owner.Remove(owner);
+        context.SaveChanges();
+    }
+
+});
+
+app.MapPut("api/owner/{id}", (DayCareContext context, Owner newOwner, int id) =>
+{
+    var owner = context.Owner.Find(id);
+
+    if (owner != null)
+    {
+        owner = newOwner;
+    }
+
+    context.SaveChanges();
+});
+
+app.MapPut("api/owner/dogs", (DayCareContext context, int dogId, int ownerId) =>
+{
+    var owner = context.Owner.Find(ownerId);
+    var dog = context.Dog.Find(dogId);
+
+    if (owner != null)
+    {
+        if (dog != null)
+        {
+            owner.Dogs.Add(dog);
+            context.SaveChanges();
+        }
+    }
+});
 
 app.Run();
 
